d8fe8c7234886c60d45641b660c83472030ebddf
raczkowskir <raczkowskir@yahoo.com>
2021-11-09 15:09:43 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import "fmt"
+
+func main()  {
+
+	var myFunnyVar = 11
+	var myFunnyVar2 = 0
+
+	if myFunnyVar == 10 {
+		fmt.Println("dziala zajebiscie")
+	} else if myFunnyVar != 11 || myFunnyVar2 == 0 {
+		fmt.Println("dziala mniej zajebiscie")
+	}
+
+	movieTitle := "God Father"
+
+
+
+	
+
+	//switch movieTitle {
+	//case "Braveheart":
+	//	fmt.Println("Cast: Mell Gibson")
+	//case "Me Irene and Me":
+	//	fmt.Println("Cast: Jim Carrey")
+	//case "Touchless":
+	//	fmt.Println("Cast: Shon Connery")
+	//case "Just Friends":
+	//	fmt.Println("Cast: Emy Smart")
+	//default:
+	//	fmt.Println("Some random actors")
+	//
+	//}
+
+	//myInt := 99
+	//
+	//var myUniversalVar interface{}
+	//
+	//
+	//fmt.Println("Napisz jakies slowo:")
+	////fmt.Scanln(myUniversalVar)
+	//fmt.Println("Jestes typem: ", myUniversalVar)
+	//myUniversalVar = 10
+	//
+	//fmt.Println("Jestes typem: ", myUniversalVar)
+	//myUniversalVar = "dupa"
+	//
+	//fmt.Println("Jestes typem: ", myUniversalVar)
+	//myUniversalVar = true
+	//
+	//fmt.Println("Jestes typem: ", myUniversalVar)
+	//myUniversalVar = nil
+	//fmt.Println("Jestes typem: ", myUniversalVar)
+	//
+	//var myBool = true
+	//var myString string = "a to ciekawe"
+	//var dupa = 0.89
+	//var ears float64 = 0.005
+	//var weirdVariable = true
+	//
+	//myInt2 := 0.1
+	//
+	//myInt2 = 1
+	//
+	//myInt2 = 0.8
+	////myInt2 = int(0.8)
+	//
+	//
+	//fmt.Println(myInt2)
+	//fmt.Println(weirdVariable)
+	//fmt.Println(dupa, ears)
+	//fmt.Println(myString, myString)
+	//
+	//fmt.Println(
+	//	myBool,
+	//	"Dzialaj dziadu!",
+	//)
+	//fmt.Printf("Moja pierwsza zmienna %v\n", myInt )
+}
+
